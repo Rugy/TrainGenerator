f92b0b3daa31db6b80c8225e77b004fd84f15f84
Rugy <andreas.heimann@hotmail.de>
2017-06-25 16:36:15 +0200

    moved aleph

---
+++ Test.pl
@@ -0,0 +1,65 @@
+This Train has 5 Wagons
+This Train is eastbound
+Wagon 0 is large
+Wagon 1 is small
+Wagon 2 is small
+Wagon 3 is large
+Wagon 4 is small
+
+This Train has 5 Wagons
+This Train is eastbound
+Wagon 0 is large
+Wagon 1 is small
+Wagon 2 is small
+Wagon 3 is large
+Wagon 4 is small
+
+This Train has 3 Wagons
+This train is westbound
+Wagon 0 is small
+Wagon 1 is small
+Wagon 2 is large
+
+This Train has 2 Wagons
+This train is westbound
+Wagon 0 is small
+Wagon 1 is small
+
+This Train has 3 Wagons
+This train is westbound
+Wagon 0 is large
+Wagon 1 is large
+Wagon 2 is small
+
+This Train has 3 Wagons
+This train is westbound
+Wagon 0 is large
+Wagon 1 is small
+Wagon 2 is small
+
+This Train has 5 Wagons
+This train is westbound
+Wagon 0 is large
+Wagon 1 is large
+Wagon 2 is large
+Wagon 3 is small
+Wagon 4 is large
+
+This Train has 4 Wagons
+This train is westbound
+Wagon 0 is large
+Wagon 1 is small
+Wagon 2 is large
+Wagon 3 is large
+
+This Train has 2 Wagons
+This train is westbound
+Wagon 0 is large
+Wagon 1 is small
+
+This Train has 3 Wagons
+This train is westbound
+Wagon 0 is large
+Wagon 1 is large
+Wagon 2 is small
+
